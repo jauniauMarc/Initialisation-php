157ca3e73bed1f6dd5566419cf8fe4335111b63a
JAUNIAU MARC <jauniau_m@JEAN-ROSTAND.lan>
2024-10-10 12:17:59 +0200

    Ex9

---
+++ Serie_1_dexercices/Serie_1/Ex8.php
@@ -0,0 +1,14 @@
+<?php
+$argument1= 5 ;
+$argument2= 5 ;
+    function multiplication($argument1,$argument2){
+        return ($argument1 * $argument2);
+    }
+?>
+<?php
+$argument1= 5 ;
+$argument2= 5 ;
+    function multiplication($argument1,$argument2){
+        return ($argument1 * $argument2);
+    }
+?>
---
+++ Serie_1_dexercices/Serie_1/Ex9.php
@@ -0,0 +1,11 @@
+<?php
+$age= 5 ;
+$age= 34 ;
+    function estIlMajeure($age){
+        if ($age >= 18)
+            return true;
+        if ($age < 18)
+            return false;
+        return $age;
+    }
+?>
